6a32c40fc79f424e8423748122257606b1f7affb
Greg Rahn <gregrahn@users.noreply.github.com>
2014-02-10 14:47:42 -0800

    remove trailing delimiter

--- dbgen/dss.h
+++ dbgen/dss.h
@@ -514,7 +514,7 @@ int dbg_print(int dt, FILE *tgt, void *data, int len, int eol);
 #define PR_MONEY(f, str) 		dbg_print(DT_MONEY, f, (void *)str, 0, 1)
 #define PR_CHR(f, str)	 		dbg_print(DT_CHR, f, (void *)str, 0, 1)
 #define  PR_STRT(fp)   /* any line prep for a record goes here */
-#define  PR_END(fp)    fprintf(fp, "\n")   /* finish the record here */
+#define PR_END(fp) {fseek(fp, -1, SEEK_CUR);fprintf(fp, "\n");}   /* finish the record here */
 
 #ifdef SSBM
 #define  PR_DATE(tgt, yr, mn, dy)	\
